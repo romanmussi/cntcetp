406e436e37569c28451f2cc0e923d907aefb08d1
romanmussi <romanmussi@gmail.com>
2013-09-24 18:32:00 -0300

    Mejorando compatibilidad para sistema de archivos con diferentes encodings (linux utf8 / win iso-8859-1)

--- app/views/helpers/file_structure_writter.php
+++ app/views/helpers/file_structure_writter.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Escribe recursivamente un array dado
  * lo anida mediante titulos h1 h2 h3
@@ -15,66 +16,62 @@
  * 
  */
 class FileStructureWritterHelper extends AppHelper {
-    
+
     /**
      * Son los indices a leer del array
      */
     var $helpers = array('Html');
-    
     var $folders = 'folders';
     var $files = 'files';
-
     // paths de cada file para poder generar su link de descarga
     var $paths = '';
     var $level = 1;
-
     var $carpeta = "";
-    
-    function write($carpeta){
-        return $this->getFileStructure( $this->getFiles($carpeta) );
+
+    function write($carpeta) {
+        return $this->getFileStructure($this->getFiles($carpeta));
     }
-    
-    function tieneArchivos($carpeta){
-        $ff = array_pop( $this->getFiles($carpeta) );
-        return (!empty($ff) && ( count($ff['files']) > 0 || count($ff['folders']) > 0 ) ) ;
+
+    function tieneArchivos($carpeta) {
+        $ff = array_pop($this->getFiles($carpeta));
+        return (!empty($ff) && ( count($ff['files']) > 0 || count($ff['folders']) > 0 ) );
     }
-    
-    function getFiles($carpeta, &$vArchivos = array()){
+
+    function getFiles($carpeta, &$vArchivos = array()) {
         // agarro el nombre de la carpeta que estoy leyendo
 
         $this->carpeta = $carpeta;
 
         $carpetaSplitted = split(DS, $carpeta);
         $folderName = array_pop($carpetaSplitted);
-        
-        $fol =& new Folder($carpeta);
+
+        $fol = & new Folder($carpeta);
         $dir = $fol->read(true, true);
         $subfolders = $dir[0];
         $files = $dir[1];
         $vArchivos[$folderName]['folders'] = array();
-        foreach ( $subfolders as $sf) {
+        foreach ($subfolders as $sf) {
             $this->getFiles($carpeta . DS . $sf, $vArchivos[$folderName]['folders']);
         }
-                
-        $vArchivos[$folderName]['files'] = $files; // 1 lee solo los archivos (creo) no lee las carpetas
-        
-        return $vArchivos;
-    }   
 
-    
+        $vArchivos[$folderName]['files'] = $files; // 1 lee solo los archivos (creo) no lee las carpetas
+
+        return $vArchivos;
+    }
+
     function getFileStructure($array) {
 
         $mainkey = key($array);
 
         // inicializa path para el level si no existe
         if (empty($this->paths[$this->level])) {
-                $this->paths[$this->level] = '';
+            $this->paths[$this->level] = '';
         }
-        $this->paths[$this->level] = $mainkey."/";
-        
+        $this->paths[$this->level] = $mainkey . "/";
+
         if (!empty($array[$mainkey][$this->files])) {
             $cantFiles = count($array[$mainkey][$this->files]);
-        } 
+        }
         if (!empty($array[$mainkey][$this->folders])) {
             $cantFolders = count($array[$mainkey][$this->folders]);
         }
@@ -82,18 +79,19 @@ class FileStructureWritterHelper extends AppHelper {
         if (!@$cantFiles && !@$cantFolders) {
             $this->paths[$this->level] = '';
             $this->level--;
-        }
-        else {
+        } else {
             echo "<ul>";
-            if (@$cantFolders){
-                foreach ($array[$mainkey][$this->folders] as $k=>$folder) {
+            if (@$cantFolders) {
+                foreach ($array[$mainkey][$this->folders] as $k => $folder) {
                     if (!empty($folder)) {
                         $this->level++;
                         $arraux = array();
                         $arraux[$k] = $folder;
-
-                        echo "<li>".utf8_decode($k);
-
+                        if ($modo_linux) {
+                            echo "<li>" . utf8_decode($k);
+                        } else {
+                            echo "<li>" . $k;
+                        }
                         $this->getFileStructure($arraux);
                     }
                 }
@@ -105,29 +103,33 @@ class FileStructureWritterHelper extends AppHelper {
                 echo "</ul>";
 
                 $this->level--;
-            }
-            elseif (!@$cantFiles) {
+            } elseif (!@$cantFiles) {
                 $this->paths[$this->level] = '';
                 $this->level--;
             }
         }
     }
-    
-    function _readFiles($array){
+
+    function _readFiles($array) {
         if (!empty($array)) {
-            foreach ($array as $val){
-                $val = utf8_decode($val);
-                echo "<li><a href='".$this->_getFileFullPath($val)."' href='_blank'>".$val."</a></li>";
+            foreach ($array as $val) {
+                if ($modo_linux) {
+                    $val = utf8_decode($val);
+                } else {
+                    $val = $val;
+                }
+                echo "<li><a href='" . $this->_getFileFullPath($val) . "' href='_blank'>" . $val . "</a></li>";
             }
         }
     }
 
     function _getFileFullPath($file) {
         $path = '';
-        for ($i=1; $i <= $this->level; $i++) {
+        for ($i = 1; $i <= $this->level; $i++) {
             $path .= $this->paths[$i];
         }
 
-        return $this->Html->url("/" . $this->carpeta. "/". $file);
+        return $this->Html->url("/" . $this->carpeta . "/" . $file);
     }
+
 }
